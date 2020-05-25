69b3d09d431351b214cb451f62a91046ec75de05
EC2 Default User <ec2-user@ip-172-31-46-230.us-east-2.compute.internal>
2020-05-25 16:20:13 +0000

    generateコマンドの生成制限、タイムゾーン追加

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,12 @@ module Insta
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.skip_routes true #ルーティングを追加しない
+      g.assets false #アセットファイルを生成しない
+      g.test_framework false #テストを生成しない
+    end
+    config.time_zone = 'Tokyo' #タイムゾーンを設定
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
