6912eee6d3a59da7cf479e6e1b5c40f479383fd8
EC2 Default User <ec2-user@ip-172-31-46-230.us-east-2.compute.internal>
2020-06-02 13:54:09 +0000

    postモデルの生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_083109) do
+ActiveRecord::Schema.define(version: 2020_06_02_134852) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.string "picture"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_05_27_083109) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
