ef704695c9d212f7c5c7ee1b84c183c8be8d0d95
EC2 Default User <ec2-user@ip-172-31-46-230.us-east-2.compute.internal>
2020-06-07 15:15:30 +0000

    postsコントローラーの改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    #Sorceryのrequire_loginで認証されない場合の処理（not_authenticatedというメソッドを実行するため）
+    def not_authenticated
+        flash[:warning]= "ログインしてください"
+        redirect_to login_path
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
-  before_action :correct_user?, only: %i[edit update destroy]
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    #N+1問題に対応
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
   end
 
   def create
+    #postに現在のユーザーの情報を付与している
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = '投稿しました'
@@ -26,11 +27,13 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    #postが現在ログインしているユーザーのpostであるときのみ編集できる
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    #postが現在ログインしているユーザーのpostであるときのみ編集できる
+    @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
       flash[:success] = '投稿を更新しました'
       redirect_to posts_path
@@ -41,7 +44,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    #postが現在ログインしているユーザーのpostであるときのみ削除できる
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     flash[:success] = '投稿を削除しました'
     redirect_to posts_path
@@ -49,16 +53,9 @@ class PostsController < ApplicationController
 
   private
 
-  def post_params
-      #picturesは配列でパラメータを受け取りたい
-    params.require(:post).permit(:content, pictures: [])
-  end
+      def post_params
+          #picturesは配列でパラメータを受け取りたい
+        params.require(:post).permit(:content, pictures: [])
+      end
 
-  def correct_user?
-    @post = Post.find(params[:id])
-    if @post.user_id != current_user.id
-      flash[:danger] = '権限がありません'
-      redirect_to root_path
-    end
-  end
 end
