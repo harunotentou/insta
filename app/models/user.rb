08ec3172117d5292eef984427c74c751eb0aab5a
EC2 Default User <ec2-user@ip-172-31-46-230.us-east-2.compute.internal>
2020-06-25 07:46:17 +0000

    relationshipモデルの生成、各種関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,16 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :post
   # コメントとの関連付け
   has_many :comments, dependent: :destroy
+  # フォローしているユーザーとの関連付け
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  # フォローされているユーザーとの関連付け
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def own?(object)
     id == object.user_id
