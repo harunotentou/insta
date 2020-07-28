5c6d98bb70262367399910fb18dd2d8b711dd26b
EC2 Default User <ec2-user@ip-172-31-46-230.us-east-2.compute.internal>
2020-07-28 22:11:37 +0000

    Userメイラーの作成

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,35 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.like_post.subject
+  #
+  def like_post(user)
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.comment_post.subject
+  #
+  def comment_post(user)
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.follow.subject
+  #
+  def follow(user)
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
