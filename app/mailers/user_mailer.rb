bac731c3fe71f0dd94228a5ba13240a9329a70f0
EC2 Default User <ec2-user@ip-172-31-46-230.us-east-2.compute.internal>
2020-07-29 10:22:39 +0000

    いいねのメール通知

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,10 +5,13 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.like_post.subject
   #
-  def like_post(user)
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def like_post
+    # .withで渡されたパラメータをインスタンス変数に代入
+    @user_to = params[:user_to]
+    @user_from = params[:user_from]
+    @post = params[:post]
+    # mailメソッドで宛先と題名を指定
+    mail(to: @user.email, subject: '#{@user_from.username}があなたの投稿にいいねしました')
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,7 +19,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.comment_post.subject
   #
-  def comment_post(user)
+  def comment_post
     @greeting = "Hi"
 
     mail to: "to@example.org"
@@ -27,7 +30,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.follow.subject
   #
-  def follow(user)
+  def follow
     @greeting = "Hi"
 
     mail to: "to@example.org"
